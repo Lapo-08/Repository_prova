259c9824d7cf8d9a0f01a64ae8d140b4a21b1910
Lapo-08 <lapomarda.davide.08@itisdalmine.edu.it>
2024-12-17 09:58:13 +0100

    Update menu.py
    update menu

--- menu.py
+++ menu.py
@@ -1,1 +1,26 @@
+def somma():
+    n1 = int(input("Inserisci il primo numero: "))
+    n2 = int(input("Inserisci il secondo numero: "))
+    print(f"{n1} + {n2} = {n1+n2}")
 
+def sottrazione():
+    n1 = int(input("Inserisci il primo numero: "))
+    n2 = int(input("Inserisci il secondo numero: "))
+    print(f"{n1} - {n2} = {n1-n2}")
+
+print("Benvenuto nella nostra colacolatrice")
+print("Inserisci l'operazione che vuoi effettuare")
+
+while True:
+    scelta = int (input(f"1. Sottrazione\n2. Somma\n0. Per uscirte\n"))
+
+    if scelta == 0:
+        break
+
+    elif scelta == 1:
+        sottrazione()
+        
+    elif scelta == 2:    
+        somma()
+    else:
+        print("Scelta non corretta")
