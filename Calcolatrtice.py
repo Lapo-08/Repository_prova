56c940a1cf92f2e43ef6a13497e06798a7417ee6
Lapo-08 <lapomarda.davide.08@itisdalmine.edu.it>
2024-12-17 09:43:10 +0100

    Update Calcolatrtice.py
    create funcions somma

--- Calcolatrtice.py
+++ Calcolatrtice.py
@@ -1,3 +1,8 @@
+def somma():
+        n1 = int(input("Inserisci il primo numero: "))
+        n2 = int(input("Inserisci il secondo numero: "))
+        print(f"{n1} + {n2} = {n1+n2}")
+
 print("Benvenuto nella nostra colacolatrice")
 print("Inserisci l'operazione che vuoi effettuare")
 
@@ -7,16 +12,15 @@ while True:
     if scelta == 0:
         break
 
-    elif scelta == 2:
-
-        n1 = int(input("Inserisci il primo numero: "))
-        n2 = int(input("Inserisci il secondo numero: "))
-        print(f"{n1} + {n2} = {n1+n2}")
-
     elif scelta == 1:
+
         n1 = int(input("Inserisci il primo numero: "))
         n2 = int(input("Inserisci il secondo numero: "))
         print(f"{n1} - {n2} = {n1-n2}")
+
+    elif scelta == 2:
+        
+    somma()
 
     else:
         print("Scelta non corretta")
