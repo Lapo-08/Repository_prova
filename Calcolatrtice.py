750eb46d77a3218d8c72fc36a5704201eae696c5
DAVIDE LAPOMARDA <lapomarda.davide.08@itisdalmine.edu.it>
2024-12-17 09:40:10 +0100

    resolve bug

--- Calcolatrtice.py
+++ Calcolatrtice.py
@@ -1,21 +1,22 @@
 print("Benvenuto nella nostra colacolatrice")
 print("Inserisci l'operazione che vuoi effettuare")
 
-scelta = -1
-while scelta != 0:
-    scelta = int (input(f"1. Sottrazione\n2. Somma\n0. Per uscirte"))
+while True:
+    scelta = int (input(f"1. Sottrazione\n2. Somma\n0. Per uscirte\n"))
 
-    if scelta == 1:
-
-        n1 = int(input("Inserisci il primo numero: "))
-        n2 = int(input("Inserisci il secondo numero: "))
-        print(f"{n1} - {n2} = {n1-n2}")
+    if scelta == 0:
+        break
 
     elif scelta == 2:
 
         n1 = int(input("Inserisci il primo numero: "))
         n2 = int(input("Inserisci il secondo numero: "))
         print(f"{n1} + {n2} = {n1+n2}")
+
+    elif scelta == 1:
+        n1 = int(input("Inserisci il primo numero: "))
+        n2 = int(input("Inserisci il secondo numero: "))
+        print(f"{n1} - {n2} = {n1-n2}")
 
     else:
         print("Scelta non corretta")
