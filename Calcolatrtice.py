79cc7339cd9ddd4fba61b9d9de6224ba9a19c3bf
DAVIDE LAPOMARDA <lapomarda.davide.08@itisdalmine.edu.it>
2024-12-17 10:01:17 +0100

    implementazione del menu

--- Calcolatrtice.py
+++ Calcolatrtice.py
@@ -8,20 +8,4 @@ def sottrazione():
     n2 = int(input("Inserisci il secondo numero: "))
     print(f"{n1} - {n2} = {n1-n2}")
 
-print("Benvenuto nella nostra colacolatrice")
-print("Inserisci l'operazione che vuoi effettuare")
-
-while True:
-    scelta = int (input(f"1. Sottrazione\n2. Somma\n0. Per uscirte\n"))
-
-    if scelta == 0:
-        break
-
-    elif scelta == 1:
-        sottrazione()
-        
-    elif scelta == 2:    
-        somma()
-    else:
-        print("Scelta non corretta")
 
--- menu.py
+++ menu.py
@@ -1,1 +1,15 @@
+import Calcolatrtice as clc
 
+while True:
+    scelta = int (input(f"1. Sottrazione\n2. Somma\n0. Per uscirte\n"))
+
+    if scelta == 0:
+        break
+
+    elif scelta == 1:
+        clc.sottrazione()
+        
+    elif scelta == 2:    
+        clc.somma()
+    else:
+        print("Scelta non corretta")
