959a5fbd1fb226a6c8a1a81058968ee0288701e7
DAVIDE LAPOMARDA <lapomarda.davide.08@itisdalmine.edu.it>
2024-12-17 09:47:24 +0100

    Implementata la funzione sottrazione

--- Calcolatrtice.py
+++ Calcolatrtice.py
@@ -1,7 +1,12 @@
 def somma():
-        n1 = int(input("Inserisci il primo numero: "))
-        n2 = int(input("Inserisci il secondo numero: "))
-        print(f"{n1} + {n2} = {n1+n2}")
+    n1 = int(input("Inserisci il primo numero: "))
+    n2 = int(input("Inserisci il secondo numero: "))
+    print(f"{n1} + {n2} = {n1+n2}")
+
+def sottrazione():
+    n1 = int(input("Inserisci il primo numero: "))
+    n2 = int(input("Inserisci il secondo numero: "))
+    print(f"{n1} - {n2} = {n1-n2}")
 
 print("Benvenuto nella nostra colacolatrice")
 print("Inserisci l'operazione che vuoi effettuare")
@@ -13,15 +18,10 @@ while True:
         break
 
     elif scelta == 1:
-
-        n1 = int(input("Inserisci il primo numero: "))
-        n2 = int(input("Inserisci il secondo numero: "))
-        print(f"{n1} - {n2} = {n1-n2}")
-
-    elif scelta == 2:
+        sottrazione()
         
-    somma()
-
+    elif scelta == 2:    
+        somma()
     else:
         print("Scelta non corretta")
 
